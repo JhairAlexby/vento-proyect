012823869c5dfa08814ee3e40069507afa2c3b12
JhairAlexby <jhair.alexby1@gmail.com>
2025-01-18 19:12:49 -0600

    add Login and Register pages; implement Label and Input components with Radix UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import LandingPage from './page/LandingPage'
+import { LoginPage } from './page/auth/LoginPage'
+import { RegisterPage } from './page/auth/RegisterPage'
 
 
 
@@ -11,6 +13,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       
       </Routes>
     </Router>
